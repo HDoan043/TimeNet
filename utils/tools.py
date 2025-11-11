2939e8c95ce963d57a25332855ce4c97001ba492
HDoan043 <165142987+HDoan043@users.noreply.github.com>
2025-11-11 16:24:11 +0700

    Add progress bar

--- utils/tools.py
+++ utils/tools.py
@@ -24,8 +24,8 @@ class ProgressBar():
             yield item
     
     def show(self):
-        percentage = round(self.i*self.bin/len(self._iteration))
-        progress = "\r|"+"="*percentage + " "*(self.bin-percentage)+f"| {percentage}% "+ self.postfix 
+        percentage = self.i/len(self._iteration)
+        progress = "\r|"+"="*round(percentage*self.bin) + " "*(self.bin-percentage)+f"| {round(percentage*100)}% "+ self.postfix 
         len_current_print = len(progress)
         end_blank = (self.len_pre_print-len_current_print) if len_current_print<self.len_pre_print else 0
         print(progress + " "*end_blank, end="")
