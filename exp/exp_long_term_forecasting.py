c9742638c09276388b086a3c8e21cd99b7843add
HDoan043 <165142987+HDoan043@users.noreply.github.com>
2025-11-17 09:29:58 +0700

    Update exp_long_term_forecasting.py

--- exp/exp_long_term_forecasting.py
+++ exp/exp_long_term_forecasting.py
@@ -198,7 +198,7 @@ class Exp_Long_Term_Forecast(Exp_Basic):
         elif total_training_time < 3600:
             total_training_time = "{} mins".format(round(total_training_time/60, 4)
         else:
-             total_training_time = "{} hours".format(round(total_training_time/3600, 4)                                      
+            total_training_time = "{} hours".format(round(total_training_time/3600, 4)                                      
         print("Total training time: {}".format(total_training_time))
         return self.model
 
