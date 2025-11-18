bb08eb6321d5025b9221ebb384454bbfe52a0968
HDoan043 <165142987+HDoan043@users.noreply.github.com>
2025-11-18 19:43:40 +0700

    Update TimesNet_update_v1.py

--- models/TimesNet_update_v1.py
+++ models/TimesNet_update_v1.py
@@ -246,7 +246,7 @@ class Model(nn.Module):
 
     def forward(self, x_enc, x_mark_enc, x_dec, x_mark_dec, mask=None):
         if self.task_name == 'long_term_forecast' or self.task_name == 'short_term_forecast':
-            dec_out = self.forecast(index, x_enc, x_mark_enc, x_dec, x_mark_dec)
+            dec_out = self.forecast(x_enc, x_mark_enc, x_dec, x_mark_dec)
             return dec_out[:, -self.pred_len:, :]  # [B, L, D]
         if self.task_name == 'imputation':
             dec_out = self.imputation(
