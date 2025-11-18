147859a6b813ee5bd068330dc8b9dc5b4e1461f1
HDoan043 <165142987+HDoan043@users.noreply.github.com>
2025-11-18 15:00:15 +0700

    Update TimesNet_update_v1.py

--- models/TimesNet_update_v1.py
+++ models/TimesNet_update_v1.py
@@ -54,31 +54,57 @@ class TimesBlockUpdate(nn.Module):
                 nn.GELU()])
         self.feedforward = nn.Sequential( *ff)
 
-    def forward(self, x_inner, x_outer):                                     # x_in: [Batch_size x period x d_model], x_out: [Batch_size x f x d_model]
-        B, P, F, N = x.size()
-      
+    def forward(self, x_inner, x_outer):                                     # x_inner: [Batch_size x period x f x d_model], 
+                                                                             # x_outer: [Batch_size x period x f x d_model]
+        B, P, F, D = x_inner.shape
+
+        x_inner = torch.reshape( x_inner, (B*F, P, D))
+        x_outer = torch.reshape( x_outer, (B*P, F, D))
+        
         # inner
         p = x_inner.shape[1]
         att_in = self.att_inner(x_inner, x_inner, x_inner)
         x_inner = x_inner + att_in
-        x_inner = self.feedforward_inner(x_inner)                            # x_in: [Batch_size x period x d_model]
+        x_inner = self.feedforward_inner(x_inner)                            # x_in: [Batch_size*f x period x d_model]
 
         # outer
         f = x_outer.shape[2]
         att_out = self.att_outer(x_outer, x_outer, x_outer)
         x_outer = x_outer + att_out
-        x_outer = self.feedforward_outer(x_outer)                            # x_out: [Batch_size x f x d_model]
+        x_outer = self.feedforward_outer(x_outer)                            # x_out: [Batch_size *period x f x d_model]
 
         # combine inner and outer
-        x = torch.cat([x_inner, x_outer], dim=1)                             # x: [Batch_size x (period + f) x d_model]
+        x_inner = torch.reshape( x_inner, (B*P*F, D))                        # x_inner: [Batch_size * period * f x d_model]
+        x_outer = torch.reshape( x_outer, (B*P*F, D))                        # x_outer: [Batch_size * period * f x d_model]
+        x = torch.cat([x_inner, x_outer], dim=0)                             # x: [2* Batch_size * period * f x d_model]
         att_combine = self.att_combine(x, x, x)
-        x = x + atta_combine                                                 # x: [Batch_size x (period + f) x d_model]
-        x = self.feedforward(x)                                              # x: [Batch_size x (period + f) x d_model]
+        x = x + atta_combine                                                 # x: [2* Batch_size * period * f x d_model]
+        x = self.feedforward(x)                                              # x: [2 * Batch_size * period * f x d_model]
 
-        new_x_inner = x[:, :p, :]
-        new_x_outer = x[:, p:, :]
+        length = x.shape[0]
+        new_x_inner = x[:length/2,:]                                         # new_x_inner: [Batch_size * period * f x d_model]
+        new_x_inner = torch.reshape(new_x_inner, (B, P, F, D))               # new_x_inner: [Batch_size x period x f x d_model]
+        new_x_outer = x[length/2:, :]                                        # new_x_outer: [Batch_size * period * f x d_model]
+        new_x_outer = torch.reshape(new_x_outer, (B, P, F, D))               # new_x_outer: [Batch_size x period x f x d_model]
+        
         return new_x_inner, new_x_outer
 
+class CombineHead(nn.Module):
+    def __init__(self, d_model):
+        super(CombineHead, self).__init__()
+        self.d_model = d_model
+        self.combine = nn.Linear( 2*d_model, d_model)
+    def forward(self, x_p, x_f):                                            # x_p: [Batch_size x period x f x d_model]
+                                                                            # x_f: [Batch_size x period x f x d_model]
+        B, P, F, D = x_p.shape
+        x_p = torch.reshape(x_p, (B*P*F, D))                                # x_p: [Batch_size * period * f x d_model]
+        x_f = torch.reshape(x_f, (B*P*F, D))                                # x_f: [Batch_size * period * f x d_model]
+        x = torch.cat([x_p, x_f], dim=1)                                    # x: [Batch_size * period * f x 2 * d_model]
+        x = self.combine(x)                                                 # x: [Batch_size * period *f x d_model]
+        x = torch.reshape( x, (B, P, F, D))                                 # x: [Batch_size x period x f x d_model]
+
+        return x
+        
 class Model(nn.Module):
     """
     Paper link: https://openreview.net/pdf?id=ju_Uqw384Oq
@@ -102,6 +128,7 @@ class Model(nn.Module):
         if self.task_name == 'long_term_forecast' or self.task_name == 'short_term_forecast':
             self.predict_linear = nn.Linear(
                 self.seq_len, self.pred_len + self.seq_len)
+            self.combine_head = CombineHead(configs.d_model)
             self.projection = nn.Linear(
                 configs.d_model, configs.c_out, bias=True)
         if self.task_name == 'imputation' or self.task_name == 'anomaly_detection':
@@ -140,14 +167,16 @@ class Model(nn.Module):
                           N).contiguous()    # out: [Batch_size x period_i x f_i x nvars]
       
         # EMBEDDING
-        # [1] - Embedding with dimension of f_i:
-        out_f = torch.mean(out, 2)                             # out_f: [Batch_size x period_i x nvars]
-        #        Embedding for inside a period
-        out_f = self.enc_embedding(out_f)                      # out_f: [Batch_size x period_i x d_model]
-        # [2] - Embedding with dimension of p_i"
-        out_p = torch.mean(out, 1)                             # out_p: [Batch_size x f_i x nvars]
-        #       Embedding across the period
-        out_p = self.enc_embedding(out_p)                      # out_p: [Batch_size x f_i x d_model]
+        B, P, F, N = out.shape
+        # [1] - Embedding with dimension of f_i: Embedding for inside a period
+        out_f = torch.reshape( out, (B*F, P, N) )              # out_f: [Batch_size*f_i x period_i x nvars]
+        out_f = self.enc_embedding(out_f)                      # out_f: [Batch_size*f_i x period_i x d_model]
+        out_f = torch.reshape( out_f, (B, P, F, -1))           # out_f: [Batch_size x period_i x f_i x d_model]
+    
+        # [2] - Embedding with dimension of p_i: Embedding across the period
+        out_p = torch.reshape( out, (B*P, F, N))               # out_p: [Batch_size*period_i x f_i x nvars]
+        out_p = self.enc_embedding(out_p)                      # out_p: [Batch_size*period_i x f_i x d_model]
+        out_p = torch.reshape( out_f, (B, P, F, -1))           # out_p: [Batch_size x period_i x f_i x d_model]
 
         # PASS IN BACKBONE
         for each in self.model:
@@ -155,11 +184,10 @@ class Model(nn.Module):
             out_f = self.layer_norm(out_f)
             out_p = self.layer_norm(out_p)
 
-        ############################################
-        # WAITING FOR A RECONSTRUCT HEAD TO RECONSTRUCT INPUT FROM OUT_F AND OUT_P
-        ############################################
+        # Combine out_f and out_p
+        enc_out = self.combine_head(out_f, out_p)              # enc_out: [Batch_size x period_i x f_i x d_model]
         # project back
-        dec_out = self.projection(enc_out)
+        dec_out = self.projection(enc_out)                     # dec_out: [Batch_size x period_i x f_i x nvars]
 
         # De-Normalization from Non-stationary Transformer
         dec_out = dec_out.mul(
