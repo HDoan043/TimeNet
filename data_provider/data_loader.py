7d301f9b51fae562d20ca2a0c32ec7d86186ebc2
HDoan043 <165142987+HDoan043@users.noreply.github.com>
2025-12-05 21:20:30 +0700

    Fix typo in frequency list value check

--- data_provider/data_loader.py
+++ data_provider/data_loader.py
@@ -286,7 +286,7 @@ class Dataset_Custom(Dataset):
         freq_df = pd.DataFrame(freq_ls)
         freq = freq_df.mode()
         for i in range(len(freq_ls)):
-            if freq_ls[i].values != freq:
+            if freq_ls[i].value != freq:
                 interupt_index.append(i)
         interupt_index.append(len(timestamps)-1)
         if len(interupt_index) != 2:
