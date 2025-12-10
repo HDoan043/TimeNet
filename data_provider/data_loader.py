c003d1bd67a0675f1d05f20dcb87f9f658069421
HDoan043 <165142987+HDoan043@users.noreply.github.com>
2025-12-10 22:32:07 +0700

    Update data_loader.py

--- data_provider/data_loader.py
+++ data_provider/data_loader.py
@@ -260,6 +260,7 @@ class Dataset_Custom(Dataset):
         if self.target in cols:
             cols.remove(self.target)
         cols.remove('date')
+        print(list(df.columns))
         if self.target in cols:
             df_raw = df_raw[['date'] + cols + [self.target]]
         else:
