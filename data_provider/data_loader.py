6ec68290d538a4befcbbeed031325b2bbaaa402e
HDoan043 <165142987+HDoan043@users.noreply.github.com>
2025-12-10 22:30:28 +0700

    Update data_loader.py

--- data_provider/data_loader.py
+++ data_provider/data_loader.py
@@ -264,7 +264,7 @@ class Dataset_Custom(Dataset):
             df_raw = df_raw[['date'] + cols + [self.target]]
         else:
             df_raw = df_raw[['date'] + cols]
-        print(cols)
+        print(list(df_raw.columns))
 
         # Split train, vali, test
         num_train = int(len(df_raw) * self.train_ratio)
