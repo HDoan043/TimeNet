ab01f2a08bb0fbf4134f1a976cc71861b9946107
HDoan043 <165142987+HDoan043@users.noreply.github.com>
2026-03-26 12:21:18 +0700

    Refactor data_loader for interrupted timestamp handling
    Refactor data loading logic to handle interrupted timestamps and improve sample indexing.

--- data_provider/data_loader.py
+++ data_provider/data_loader.py
@@ -315,7 +315,6 @@ class Dataset_Custom(Dataset):
         mod_freq_series = (df["timestamp"] - df["previous_timestamp"]).mode()/np.timedelta64(1, "m")
         mod_freq = str(mod_feq_series.iloc[0]) + "min"
 
-        # ============================NOT COMPLETED YET ==============================
         # Inspect interupt
         full_timestamp_range = np.array(pd.date_range(start=timestamps[0], end=timestamps[-1], freq=mod_freq))
         timestamps = np.array(timestamps)
@@ -331,86 +330,89 @@ class Dataset_Custom(Dataset):
         index_col = full_timestamp_df_with_index["index"].values
         interrupted_index = []
         stack = []
-        for i, index in enumerate(index_col):
-            if not index and not len(stack):
+        for index, is_interrupted in list(zip(index_col, interrupted_timestamp)):
+            if not is_interrupted:
                 stack.append(index)
-            elif not index and len(stack)==1:
-                interrupted_index.append(stack[-1])
-                stack.pop()
-            
-        # ==================================================================================
-    #     interupt_index = 
+            elif len(stack):
+                interrupted_index.append(stack)
+                stack.clear()
+                
+        if len(interupt_index):
+            print("[ℹ️] The sequence for {} is interupted at {} indexes: {}".format(self.flag, 
+                                                                                      len(interrupted_index), 
+                                                                                      [each[0] for each in interrupted_index]))
+        else:
+            print("[INFO] The sequence for {} is continous".format(self.flag))
+
+        # Get the possible indexes: 
+        #     a training sample is a window of timestamps, 
+        #     the index of training sample is the index of the beginning timestamp in window
         
-    #     if len(interupt_index) != 2:
-    #         print("[INFO] The sequence for {} is interupted at {} indexes: {}".format(self.flag, self.flag, interupt_index[1:-1]))
-    #     else:
-    #         print("[INFO] The sequence for {} is continous".format(self.flag))
-            
-    #     possible_index = []
-    #     sample_length = self.seq_len + self.pred_len if self.task_name == "long_term_forecasting" else self.win_size
-    #     for i in range(len(interupt_index)-1):
-    #         # if there are enough continous elements
-    #         if interupt_index[i+1] - interupt_index[i] +1 >= sample_length:
-    #             if self.task_name == "long_term_forecasting":
-    #                 possible_index.extend(list(range(interupt_index[i], interupt_index[i+1] + 1 - sample_length)))
-    #             else:
-    #                 possible_index.extend(list(range(interupt_index[i], interupt_index[i+1] + 1 - sample_length, self.step)))
-    #     self.possible_index = possible_index
-    #     self.possible_timestamps = pd.DataFrame([timestamps[index:index+sample_length] for index in possible_index])
-    #     print("[INFO] Number of {} samples: {}".format(self.flag, len(possible_index)))
-        
-    #     if self.timeenc == 0:
-    #         df_stamp['month'] = df_stamp.date.apply(lambda row: row.month, 1)
-    #         df_stamp['day'] = df_stamp.date.apply(lambda row: row.day, 1)
-    #         df_stamp['weekday'] = df_stamp.date.apply(lambda row: row.weekday(), 1)
-    #         df_stamp['hour'] = df_stamp.date.apply(lambda row: row.hour, 1)
-    #         data_stamp = df_stamp.drop(['date'], 1).values
-    #     elif self.timeenc == 1:
-    #         data_stamp = time_features(pd.to_datetime(df_stamp['date'].values), freq=self.freq)
-    #         data_stamp = data_stamp.transpose(1, 0)
+        possible_index = []
+        sample_length = self.seq_len + self.pred_len if self.task_name == "long_term_forecasting" else self.win_size
+        for continous_indexes in interrupetd_index:
+            if len(continous_indexes) >= sample_length:
+                max_start_index = len(continuous_indexes) - sample_length + 1
+                step_index_index = range(0, max_start_index, self.step)
+                step_index = [continuous_indexes[index] for index in step_index_index]
+                possible_index.extend(step_index)
+        self.possible_index = possible_index
+        self.possible_timestamps = pd.DataFrame([timestamps[index:index+sample_length] for index in possible_index])
+        print("[INFO] Number of {} samples: {}".format(self.flag, len(possible_index)))
 
-    #     self.data_x = data[border1:border2]
-    #     self.data_y = y[border1:border2]
+        # ========================= ENCODE TIMELABEL ===============================
+        if self.timeenc == 0:
+            df_stamp['month'] = df_stamp.date.apply(lambda row: row.month, 1)
+            df_stamp['day'] = df_stamp.date.apply(lambda row: row.day, 1)
+            df_stamp['weekday'] = df_stamp.date.apply(lambda row: row.weekday(), 1)
+            df_stamp['hour'] = df_stamp.date.apply(lambda row: row.hour, 1)
+            data_stamp = df_stamp.drop(['date'], 1).values
+        elif self.timeenc == 1:
+            data_stamp = time_features(pd.to_datetime(df_stamp['date'].values), freq=self.freq)
+            data_stamp = data_stamp.transpose(1, 0)
 
-    #     if self.set_type == 0 and self.args.augmentation_ratio > 0:
-    #         self.data_x, self.data_y, augmentation_tags = run_augmentation_single(self.data_x, self.data_y, self.args)
+        self.data_x = data[border1:border2]
+        self.data_y = y[border1:border2]
 
-    #     self.data_stamp = data_stamp
+        if self.set_type == 0 and self.args.augmentation_ratio > 0:
+            self.data_x, self.data_y, augmentation_tags = run_augmentation_single(self.data_x, self.data_y, self.args)
 
-    # def __getitem__(self, index):
-    #     index = self.possible_index[index]
-    #     s_begin = index
-    #     if self.task_name == "long_term_forecasting":
-    #         s_end = s_begin + self.seq_len
-    #         r_begin = s_end - self.label_len
-    #         r_end = r_begin + self.label_len + self.pred_len
+        self.data_stamp = data_stamp
+
+    def __getitem__(self, index):
+        index = self.possible_index[index]
+        s_begin = index
+        if self.task_name == "long_term_forecasting":
+            s_end = s_begin + self.seq_len
+            r_begin = s_end - self.label_len
+            r_end = r_begin + self.label_len + self.pred_len
     
-    #         seq_x = self.data_x[s_begin:s_end]
-    #         seq_y = self.data_y[r_begin:r_end]
-    #         seq_x_mark = self.data_stamp[s_begin:s_end]
-    #         seq_y_mark = self.data_stamp[r_begin:r_end]
+            seq_x = self.data_x[s_begin:s_end]
+            seq_y = self.data_y[r_begin:r_end]
+            seq_x_mark = self.data_stamp[s_begin:s_end]
+            seq_y_mark = self.data_stamp[r_begin:r_end]
     
-    #         return seq_x, seq_y, seq_x_mark, seq_y_mark
-    #     else:
-    #         x_index_start = index
-    #         x_index_end = index + self.win_size
-    #         y_index_start = index 
-    #         y_index_end = index + self.win_size
+            return seq_x, seq_y, seq_x_mark, seq_y_mark
+        else:
+            x_index_start = index
+            x_index_end = index + self.win_size
+            y_index_start = index 
+            y_index_end = index + self.win_size
 
-    #         seq_x = self.data_x[x_index_start: x_index_end]
-    #         seq_y = self.data_y[y_index_start: y_index_end]
+            seq_x = self.data_x[x_index_start: x_index_end]
+            seq_y = self.data_y[y_index_start: y_index_end]
 
-    #         return seq_x, seq_y
+            return seq_x, seq_y
     
-    # def __len__(self):
-    #     # return len(self.data_x) - self.seq_len - self.pred_len + 1
-    #     return len(self.possible_index)
+    def __len__(self):
+        # return len(self.data_x) - self.seq_len - self.pred_len + 1
+        return len(self.possible_index)
 
-    # def get_timestamps(self):
-    #     return self.possible_timestamps
+    def get_timestamps(self):
+        return self.possible_timestamps
 
-    # def inverse_transform(self, data):
-    #     return self.scaler.inverse_transform(data)
+    def inverse_transform(self, data):
+        return self.scaler.inverse_transform(data)
 
     # def get_corr_matrix(self):
     #     df_raw = pd.read_csv(os.path.join(self.root_path, self.data_path))
