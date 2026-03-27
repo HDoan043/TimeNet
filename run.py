b3e7e298cbf83da45092a56fa6ffb009c283d3e0
HDoan043 <165142987+HDoan043@users.noreply.github.com>
2026-03-28 00:54:13 +0700

    Update run.py

--- run.py
+++ run.py
@@ -55,7 +55,7 @@ if __name__ == '__main__':
 
     # anomaly detection task
     parser.add_argument('--anomaly_ratio', type=float, default=0.25, help='prior anomaly ratio (%%)')
-    parser.add_argument('--encode_timestamps', nargs="+" , default=["month", "day", "weekday", "hour", "minute"], help="select units timestamps for encoding")
+    parser.add_argument('--encode_timestamps', nargs="+" , default=["month", "day", "weekday", "hour"], help="select units timestamps for encoding")
 
     # model define
     parser.add_argument('--expand', type=int, default=2, help='expansion factor for Mamba')
