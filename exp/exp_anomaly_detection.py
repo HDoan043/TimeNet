2216dfa01b4fe911aee1366547dd46b00d31006a
HDoan043 <165142987+HDoan043@users.noreply.github.com>
2026-03-27 23:12:23 +0700

    Update exp_anomaly_detection.py

--- exp/exp_anomaly_detection.py
+++ exp/exp_anomaly_detection.py
@@ -47,7 +47,7 @@ class Exp_Anomaly_Detection(Exp_Basic):
         with torch.no_grad():
             for i, (batch_x, _, batch_x_mark, _) in enumerate(vali_loader):
                 batch_x = batch_x.float().to(self.device)
-                batch_x_mark = batch_x_mark.to(self.device) if self.args.encode_timestamps else None
+                batch_x_mark = batch_x_mark.to(self.device) 
 
                 if self.args.model.lower() == "timesnetv2":
                     outputs = self.model(batch_x, None, None, None, corr_matrix)
@@ -104,7 +104,7 @@ class Exp_Anomaly_Detection(Exp_Basic):
                 iter_count += 1
                 model_optim.zero_grad()
                 batch_x = batch_x.float().to(self.device)
-                batch_x_mark = batch_x_mark.to(self.device) if self.args.encode_timestamps else None
+                batch_x_mark = batch_x_mark.to(self.device) 
 
                 if self.args.model.lower() == "timesnetv2":
                     outputs = self.model(batch_x, None, None, None, corr_matrix)
@@ -191,7 +191,7 @@ class Exp_Anomaly_Detection(Exp_Basic):
         with torch.no_grad():
             for i, (batch_x, batch_y, batch_x_mark, batch_y_mark) in enumerate(train_loader):
                 batch_x = batch_x.float().to(self.device)
-                batch_x_mark = batch_x_mark.to(self.device) if self.args.encode_timestamps else None
+                batch_x_mark = batch_x_mark.to(self.device) 
                 # reconstruction
                 if self.args.model.lower() == "timesnetv2":
                     outputs = self.model(batch_x, None, None, None, corr_matrix)
@@ -209,7 +209,7 @@ class Exp_Anomaly_Detection(Exp_Basic):
         test_labels = []
         for i, (batch_x, batch_y, batch_x_mark, batch_y_mark) in enumerate(test_loader):
             batch_x = batch_x.float().to(self.device)
-            batch_x_mark = batch_x_mark.to(self.device) if self.args.encode_timestamps else None
+            batch_x_mark = batch_x_mark.to(self.device) 
             # reconstruction
             if self.args.model.lower() == "timesnetv2":
                 outputs = self.model(batch_x, None, None, None, corr_matrix)
@@ -301,7 +301,7 @@ class Exp_Anomaly_Detection(Exp_Basic):
         with torch.no_grad():
             for i, (batch_x, batch_y, batch_x_mark, batch_y_mark) in enumerate(train_loader):
                 batch_x = batch_x.float().to(self.device)
-                batch_x_mark = batch_x_mark.to(self.device) if self.args.encode_timestamps else None
+                batch_x_mark = batch_x_mark.to(self.device) 
                 # reconstruction
                 if self.args.model.lower() == "timesnetv2":
                     outputs = self.model(batch_x, None, None, None, corr_matrix)
@@ -317,7 +317,7 @@ class Exp_Anomaly_Detection(Exp_Basic):
         gt_labels = []
         for i, (batch_x, batch_y, batch_x_mark, batch_y_mark) in enumerate(infer_loader):
             batch_x = batch_x.float().to(self.device)
-            batch_x_mark = batch_x_mark.to(self.device) if self.args.encode_timestamps else None
+            batch_x_mark = batch_x_mark.to(self.device)
             # reconstruction
             if self.args.model.lower() == "timesnetv2":
                 outputs = self.model(batch_x, None, None, None, corr_matrix)
@@ -338,7 +338,7 @@ class Exp_Anomaly_Detection(Exp_Basic):
         attens_energy = []
         for i, (batch_x, _, _, _) in enumerate(full_loader):
             batch_x = batch_x.float().to(self.device)
-            batch_x_mark = batch_x_mark.to(self.device) if self.args.encode_timestamps else None
+            batch_x_mark = batch_x_mark.to(self.device)
             # reconstruction
             if self.args.model.lower() == "timesnetv2":
                 outputs = self.model(batch_x, None, None, None, corr_matrix)
