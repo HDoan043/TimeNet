7a2e2e18ad07d7cdf9e8b837b6b4d122423e0561
HDoan043 <165142987+HDoan043@users.noreply.github.com>
2026-03-27 23:44:21 +0700

    Update data_factory.py

--- data_provider/data_factory.py
+++ data_provider/data_factory.py
@@ -44,7 +44,6 @@ def data_provider(args, flag):
             target=args.target,
             timeenc=timeenc,
             freq=freq,
-            encode_timestamps=args.encode_timestamps
             seasonal_patterns=args.seasonal_patterns,
             train_ratio = train_ratio,
             test_ratio = test_ratio
