e29fa6a3a0ff2d6f37a3df8b4a73ff9b9f62e198
HDoan043 <165142987+HDoan043@users.noreply.github.com>
2026-03-27 12:03:04 +0700

    Simplify timestamp range and missing detection logic
    Refactor timestamp handling to simplify missing timestamp detection.

--- data_provider/data_loader.py
+++ data_provider/data_loader.py
@@ -316,10 +316,9 @@ class Dataset_Custom(Dataset):
         mod_freq = str(mod_freq_series.iloc[0]) + "min"
 
         # Inspect interupt
-        full_timestamp_range = np.array(pd.to_datetime(pd.date_range(start=timestamps[0], end=timestamps[-1], freq=mod_freq)))
-        timestamps = np.array(timestamps)
-        interrupted_timestamp = np.array([each not in timestamps for each in full_timestamp_range])*1
-        full_timestamp_df = pd.DataFrame({"timestamps": full_timestamp_range, "missing_timestamps": interrupted_timestamp})
+        full_timestamp_range = pd.to_datetime(pd.date_range(start=timestamps[0], end=timestamps[-1], freq=mod_freq)
+        full_timestamp_df = pd.DataFrame({"timestamps": full_timestamp_range})
+        full_timestamp_df["missing_timestamps"] = ~full_timestamp_df["timestamps"].isin(timestamps)*1
         timestamps_df_with_index = pd.DataFrame({"index": df_stamp.index, "timestamps": timestamps})
         full_timestamp_df_with_index = pd.merge(
             full_timestamp_df,
@@ -327,6 +326,7 @@ class Dataset_Custom(Dataset):
             on="timestamps",
             how="right"
         )
+        interrupted_timestamp = full_timestamp_df["missing_timestamps"].values
         print(full_timestamp_df_with_index.describe())
         print(full_timestamp_range[3000:3002])
         print(timestamps[3000:3002])
