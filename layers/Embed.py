244b55dca89c52994c0b40cb9780fcd238d30b60
HDoan043 <165142987+HDoan043@users.noreply.github.com>
2026-03-27 12:37:37 +0700

    Modify Conv1d initialization in TokenEmbedding
    Commented out the padding_mode parameter in the Conv1d layer.

--- layers/Embed.py
+++ layers/Embed.py
@@ -31,7 +31,9 @@ class TokenEmbedding(nn.Module):
         super(TokenEmbedding, self).__init__()
         padding = 1 if torch.__version__ >= '1.5.0' else 2
         self.tokenConv = nn.Conv1d(in_channels=c_in, out_channels=d_model,
-                                   kernel_size=3, padding=padding, padding_mode='circular', bias=False)
+                                   kernel_size=3, padding=padding, 
+                                   # padding_mode='circular', 
+                                   bias=False)
         for m in self.modules():
             if isinstance(m, nn.Conv1d):
                 nn.init.kaiming_normal_(
