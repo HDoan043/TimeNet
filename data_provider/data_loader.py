9a7ce52442ae3624441d12252e8242e127b22452
HDoan043 <165142987+HDoan043@users.noreply.github.com>
2026-03-27 12:06:07 +0700

    Fix DataFrame creation for full timestamps

--- data_provider/data_loader.py
+++ data_provider/data_loader.py
@@ -317,7 +317,7 @@ class Dataset_Custom(Dataset):
 
         # Inspect interupt
         full_timestamp_range = pd.date_range(start=timestamps[0], end=timestamps[-1], freq=mod_freq)
-        full_timestamp_df = pd.DataFrame({"timestamps": full_timestamp_range.dt})
+        full_timestamp_df = pd.DataFrame({"timestamps": full_timestamp_range})
         full_timestamp_df["missing_timestamps"] = ~full_timestamp_df["timestamps"].isin(timestamps)*1
         timestamps_df_with_index = pd.DataFrame({"index": df_stamp.index, "timestamps": timestamps})
         full_timestamp_df_with_index = pd.merge(
