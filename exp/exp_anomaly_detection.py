5b2a3570379c4058d94c4d92edd1152665e9081e
HDoan043 <165142987+HDoan043@users.noreply.github.com>
2026-03-27 13:39:05 +0700

    Add print statements for validation loader details
    Added print statements to display validation loader information.

--- exp/exp_anomaly_detection.py
+++ exp/exp_anomaly_detection.py
@@ -68,6 +68,9 @@ class Exp_Anomaly_Detection(Exp_Basic):
         train_data, train_loader = self._get_data(flag='train')
         vali_data, vali_loader = self._get_data(flag='val')
         test_data, test_loader = self._get_data(flag='test')
+        print("vali loader")
+        print(len(vali_loader))
+        print(vali_loader[0][0].shape)
 
         corr_matrix = train_data.get_corr_matrix()
         corr_matrix = torch.tensor(corr_matrix, dtype = torch.float32, device = self.device)
