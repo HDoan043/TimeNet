e1887f936b5990e652d8eaed019782dded9de530
HDoan043 <165142987+HDoan043@users.noreply.github.com>
2026-03-28 22:33:18 +0700

    Update Embed.py

--- layers/Embed.py
+++ layers/Embed.py
@@ -258,14 +258,14 @@ class DataEmbedding_inverted(nn.Module):
 
 
 class DataEmbedding_wo_pos(nn.Module):
-    def __init__(self, c_in, d_model, embed_type='fixed', freq='h', dropout=0.1):
+    def __init__(self, c_in, d_model, embed_type='fixed', freq='h', dropout=0.1, encode_timestamps  = ["month", "day", "weekday", "hour"] ):
         super(DataEmbedding_wo_pos, self).__init__()
 
         self.value_embedding = TokenEmbedding(c_in=c_in, d_model=d_model)
         self.position_embedding = PositionalEmbedding(d_model=d_model)
         self.temporal_embedding = TemporalEmbedding(d_model=d_model, embed_type=embed_type,
-                                                    freq=freq) if embed_type != 'timeF' else TimeFeatureEmbedding(
-            d_model=d_model, embed_type=embed_type, freq=freq)
+                                                    freq=freq, encode_timestamps = encode_timestamps) if embed_type != 'timeF' \
+                                                    else TimeFeatureEmbedding(d_model=d_model, embed_type=embed_type, freq=freq)
         self.dropout = nn.Dropout(p=dropout)
 
     def forward(self, x, x_mark):
