821016e628815ea8040d431d713d52a6dd6f2de4
HDoan043 <165142987+HDoan043@users.noreply.github.com>
2026-03-29 13:11:44 +0700

    Clean up TimesNet.py by removing comments
    Removed commented-out print statements to clean up the code.

--- models/TimesNet.py
+++ models/TimesNet.py
@@ -40,18 +40,11 @@ class TimesBlock(nn.Module):
     def forward(self, x):
         B, T, N = x.size()
         total_time = time.time()
-        ###############################################
-        # print("***TimesBlock")
-        # print("---- FFT ----")
+    
         fft_time = time.time()
-        ###############################################
         period_list, period_weight = FFT_for_Period(x, self.k)
-        ###############################################
         fft_time = time.time() - fft_time
-        # print("    ~ FFT time: {}s".format(fft_time))
-        # print()
-        # print("---- Loop for each period ----")
-        ###############################################
+
         res = []
         transform_time = 0
         inception_time = 0
@@ -69,51 +62,33 @@ class TimesBlock(nn.Module):
                 out = x
             # reshape
             out = out.reshape(B, length // period, period,N).permute(0, 3, 1, 2).contiguous()
-            #############################################
             trans_time = time.time() - trans_time
             transform_time += trans_time
-            # print("    ~ Transform 1D to 2D: {}s".format(transform_time))
-            #############################################
             # 2D conv: from 1d Variation to 2d Variation
             incep_time = time.time()
             out = self.conv(out)
-            #############################################
             incep_time = time.time() - incep_time
             inception_time += incep_time
-            # print("    ~ Inception block time: {}s".format(inception_time))
-            #############################################
             # reshape back
             reshape_time = time.time()
             out = out.permute(0, 2, 3, 1).reshape(B, -1, N)
             res.append(out[:, :self.sample_length, :])
-            #############################################
+            
             reshape_time = time.time() - reshape_time
             reshape_back_time += reshape_time
-            # print("    ~ Reshape back time: {}s".format(reshape_back_time))
-            #############################################
-        #############################################
-        # print("----Combine----")
+
         combine_time = time.time()
-        #############################################
         res = torch.stack(res, dim=-1)
         # adaptive aggregation
         period_weight = F.softmax(period_weight, dim=1)
         period_weight = period_weight.unsqueeze(
             1).unsqueeze(1).repeat(1, T, N, 1)
         res = torch.sum(res * period_weight, -1)
-        #############################################
         combine_time = time.time() - combine_time
-        # print("    ~ Combine time: {}s".format(combine_time))
-        #############################################
         # residual connection
         res = res + x
         total_time = time.time() - total_time
-        # print("="*50)
-        # print("_ FFT: {}%".format(round(fft_time*100/total_time)))
-        # print("_ Transform: {}%".format(round(transform_time*100/total_time)))
-        # print("_ Inception: {}%".format(round(inception_time*100/total_time)))
-        # print("_ Reshape back: {}%".format(round(reshape_back_time*100/total_time)))
-        # print("_ Combine: {}%".format(round(combine_time*100/total_time)))
+        
         return res
 
 
@@ -154,77 +129,41 @@ class Model(nn.Module):
     def forecast(self, x_enc, x_mark_enc, x_dec, x_mark_dec):
         total_time = time.time()
         # Normalization from Non-stationary Transformer
-        #############################################
-        # print("="*25 + "[1]-NORMALIZATION"+"="*25)
         norm_time = time.time()
-        #############################################
         means = x_enc.mean(1, keepdim=True).detach()
         x_enc = x_enc.sub(means)
         stdev = torch.sqrt(
             torch.var(x_enc, dim=1, keepdim=True, unbiased=False) + 1e-5)
         x_enc = x_enc.div(stdev)
 
-        ##############################################
         norm_time = time.time() - norm_time
-        # print("- Normalization time: {}s".format(norm_time))
-        # print()
-        ##############################################
         # embedding
-        ##############################################
-        # print("="*25 + "[2]-EMBEDDING" + "="*25)
         embed_time = time.time()
-        ##############################################
         enc_out = self.enc_embedding(x_enc, x_mark_enc)  # [B,T,C]
         enc_out = self.predict_linear(enc_out.permute(0, 2, 1)).permute(
             0, 2, 1)  # align temporal dimension
-        ##############################################
+    
         embed_time = time.time() - embed_time
-        # print("- Embedding time: {}s".format(embed_time))
-        ##############################################
         # TimesNet
-        ##############################################
-        # print()
-        # print("="*25 + "[3]-TIMESBLOCK LAYERS" + "="*25)
         timesblock_time = time.time()
-        ##############################################
         for i in range(self.layer):
             enc_out = self.layer_norm(self.model[i](enc_out))
         timesblock_time = time.time() - timesblock_time
         # project back
-        ##############################################
-        # print()
-        # print("="*25 + "[4]-PROJECTION BACK" +"="*25)
         project_back_time = time.time()
-        ##############################################
         dec_out = self.projection(enc_out)
-        #############################################
         project_back_time = time.time() - project_back_time
-        # print("- Project back time: {}s".format(project_back_time))
-        #############################################
 
         # De-Normalization from Non-stationary Transformer
-        #############################################
-        # print()
-        # print("="*25 + "[5]-DE NORMALIZATION" + "="*25)
         denorm_time = time.time()
-        #############################################
         dec_out = dec_out.mul(
                   (stdev[:, 0, :].unsqueeze(1).repeat(
                       1, self.pred_len + self.seq_len, 1)))
         dec_out = dec_out.add(
                   (means[:, 0, :].unsqueeze(1).repeat(
                       1, self.pred_len + self.seq_len, 1)))
-        #############################################
         denorm_time = time.time() - denorm_time
-        # print("- De normalization time: {}s".format(denorm_time))
-        #############################################
-        total_time = time.time() - total_time
-        # print("="*50)
-        # print("_ Normalization: {}%".format(round(norm_time*100/total_time)))
-        # print("_ Embedding: {}%".format(round(embed_time*100/total_time)))
-        # print("_ TimesBlocks: {}%".format(round(timesblock_time*100/total_time)))
-        # print("_ Project Back: {}%".format(round(project_back_time*100/total_time)))
-        # print("_ De normalization: {}%".format(round(denorm_time*100/total_time)))
+    
         return dec_out
 
     def imputation(self, x_enc, x_mark_enc, x_dec, x_mark_dec, mask):
