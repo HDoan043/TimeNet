b8c323f9200a24f25844f35d5832cf9a7e2c720a
HDoan043 <165142987+HDoan043@users.noreply.github.com>
2026-03-30 10:15:00 +0700

    Fix typo in variable 'settings' to 'setting'

--- exp/exp_anomaly_detection.py
+++ exp/exp_anomaly_detection.py
@@ -175,7 +175,7 @@ class Exp_Anomaly_Detection(Exp_Basic):
         
         if test:
             print('loading model')
-            checkpoint_path = os.path.join(self.args.checkpoints, settings, 'checkpoint.pth')
+            checkpoint_path = os.path.join(self.args.checkpoints, setting, 'checkpoint.pth')
             if os.path.exists(checkpoint_path):
                 self.model.load_state_dict(torch.load(checkpoint_path))
             else:
