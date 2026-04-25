f63364c0d01e25df991d9c67b83f6bc587964f34
HDoan043 <165142987+HDoan043@users.noreply.github.com>
2026-04-25 09:16:41 +0700

    Refactor LSTMAE constructor to use configs

--- models/LSTMAE.py
+++ models/LSTMAE.py
@@ -1,17 +1,17 @@
 import torch.nn as nn
 
 class LSTMAE(nn.Module):
-    def __init__(self, enc_in, d_model, e_layers):
+    def __init__(self, configs):
         super().__init__()
 
         self.encoder = nn.LSTM(
-            input_size=enc_in,
-            hidden_size=d_model,
-            num_layers=e_layers,
+            input_size=configs.enc_in,
+            hidden_size=configs.d_model,
+            num_layers=configs.e_layers,
             batch_first=True
         )
 
-        self.decoder = nn.Linear(d_model, enc_in)
+        self.decoder = nn.Linear(configs.d_model, configs.enc_in)
 
     def forward(self, x):
         # x: [B, T, D]
