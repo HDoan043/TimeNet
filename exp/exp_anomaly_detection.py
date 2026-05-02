8cafb5579eded4c3968b5e3cd77947dcd3a0e344
HDoan043 <165142987+HDoan043@users.noreply.github.com>
2026-05-02 17:27:14 +0700

    Update logging and early stopping for validation metrics

--- exp/exp_anomaly_detection.py
+++ exp/exp_anomaly_detection.py
@@ -148,14 +148,14 @@ class Exp_Anomaly_Detection(Exp_Basic):
 
             print("Epoch: {} cost time: {}".format(epoch + 1, time.time() - epoch_time))
             train_loss = np.average(train_loss)
-            vali_loss = self.vali(vali_data, vali_loader, corr_matrix, criterion)
-            test_loss = self.vali(test_data, test_loader, corr_matrix, criterion)
+            # vali_loss = self.vali(vali_data, vali_loader, corr_matrix, criterion)
+            _, _, _, vali_best_f1, _ = self.test(test_data, test_loader, corr_matrix, criterion)
 
-            print("Epoch: {0}, Steps: {1} | Train Loss: {2:.7f} Vali Loss: {3:.7f} Test Loss: {4:.7f}".format(
-                epoch + 1, train_steps, train_loss, vali_loss, test_loss))
-            early_stopping(vali_loss, self.model, path)
+            print("Epoch: {0}, Steps: {1} | Train Loss: {2:.7f} Vali best f1: {3:.7f}".format(
+                epoch + 1, train_steps, train_loss, vali_best_f1))
+            early_stopping(vali_best_f1, self.model, path)
             if trial:
-                trial.report(vali_loss, epoch)
+                trial.report(vali_best_f1, epoch)
                 if trial.should_prune():
                     raise optuna.exceptions.TrialPruned()
                     
