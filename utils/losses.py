72cc56e28b41507317df0b4e93ac24a1e5ea7b79
HDoan043 <165142987+HDoan043@users.noreply.github.com>
2026-05-06 22:22:09 +0700

    Refactor loss calculation by removing debug prints
    Removed print statements for full_pos_mask and added print statements for recon_loss and loss.mean.

--- utils/losses.py
+++ utils/losses.py
@@ -142,7 +142,6 @@ class NTXentLoss(nn.Module):
             pos_mask.diagonal(offset=-i).fill_(1)
         neg_mask = t.zeros((B,B), device=sim.device)                             # neg_mask: [B, B]
         full_pos_mask = t.cat([pos_anchor_mask, pos_mask, neg_mask], dim=1)      # full_pos_mask: [B, 3B], full_pos_mask[i,j] = 1 if sample[j] is a positive sample of anchor[i], = 0 else
-        print(full_pos_mask)
         full_pos_mask = full_pos_mask.bool()
         logits = sim - sim.max(dim=1, keepdim=True)[0]
         exp_sim = t.exp(logits)
@@ -150,7 +149,6 @@ class NTXentLoss(nn.Module):
         pos_sum = pos_exp.sum(dim=1)    
         
         # denominator
-        print(full_pos_mask)
         full_neg_mask = ~full_pos_mask
         full_neg_mask.diagonal(offset=0).fill_(0)
         weights = t.ones_like(exp_sim[idx], device=exp_sim.device)               # weights: [B, 3B]
@@ -169,5 +167,6 @@ class NTXentLoss(nn.Module):
         denom = (exp_sim[idx]*weights).sum(dim=1)                                    # denom: [B]
 
         loss = -t.log(pos_sum / denom)
-
+        print(recon_loss.item())
+        print(loss.mean.item())
         return recon_loss + loss.mean()
