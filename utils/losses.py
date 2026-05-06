7e32c18f80ecd2725a93bb38e8ae5ec1ceef80be
HDoan043 <165142987+HDoan043@users.noreply.github.com>
2026-05-06 20:30:36 +0700

    Log device info for tensors in losses.py
    Added print statement to log device information for tensors.

--- utils/losses.py
+++ utils/losses.py
@@ -104,7 +104,8 @@ class NTXentLoss(nn.Module):
         pos_idx = pos_idx.to(x.device)
         neg_idx = neg_idx.to(x.device)
         attn_pooling = attn_pooling.to(x.device)
-        
+
+        print(f"MSE: {self.mse.device}, x: {x.device}, x_hat: {x_hat.device}, idx: {idx.device}, pos_idx: {pos_idx.device}, neg_idx: {neg_idx.device}")
         # reconstruct loss (anchor only)
         B = idx.shape[0]
         recon_loss = self.mse(x[idx], x_hat[idx])
