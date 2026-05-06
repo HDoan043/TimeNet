86f73d490a203babefc7731d1e232c8c4acb4098
HDoan043 <165142987+HDoan043@users.noreply.github.com>
2026-05-06 20:33:22 +0700

    Optimize forward method in losses.py
    Removed unnecessary device transfer for mse and updated print statement.

--- utils/losses.py
+++ utils/losses.py
@@ -99,13 +99,12 @@ class NTXentLoss(nn.Module):
 
     def forward(self, x, x_hat, z, idx, pos_idx, neg_idx, labels, attn_pooling):
         # same device
-        self.mse = self.mse.to(x.device)
         idx = idx.to(x.device)
         pos_idx = pos_idx.to(x.device)
         neg_idx = neg_idx.to(x.device)
         attn_pooling = attn_pooling.to(x.device)
 
-        print(f"MSE: {self.mse.device}, x: {x.device}, x_hat: {x_hat.device}, idx: {idx.device}, pos_idx: {pos_idx.device}, neg_idx: {neg_idx.device}")
+        print(f"x: {x.device}, x_hat: {x_hat.device}, idx: {idx.device}, pos_idx: {pos_idx.device}, neg_idx: {neg_idx.device}")
         # reconstruct loss (anchor only)
         B = idx.shape[0]
         recon_loss = self.mse(x[idx], x_hat[idx])
