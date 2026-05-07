86f1624de59b8f4b82705be09871ffcc7c5a63e5
HDoan043 <165142987+HDoan043@users.noreply.github.com>
2026-05-07 18:43:28 +0700

    Add attention layer to TimesNet model

--- models/TimesNet.py
+++ models/TimesNet.py
@@ -112,6 +112,7 @@ class Model(nn.Module):
                                            configs.dropout, configs.encode_timestamps)
         self.layer = configs.e_layers
         self.layer_norm = nn.LayerNorm(configs.d_model)
+        self.attn = nn.Linear(configs.d_model, 1)
 
         if self.task_name == 'long_term_forecast' or self.task_name == 'short_term_forecast':
             self.predict_linear = nn.Linear(
@@ -126,8 +127,6 @@ class Model(nn.Module):
             self.dropout = nn.Dropout(configs.dropout)
             self.projection = nn.Linear(
                 configs.d_model * configs.seq_len, configs.num_class)
-        if configs.contrastive == 1:
-            self.attn = nn.Linear(configs.d_model, 1)
             
     def forecast(self, x_enc, x_mark_enc, x_dec, x_mark_dec):
         total_time = time.time()
@@ -222,9 +221,9 @@ class Model(nn.Module):
         dec_out = dec_out.add(
                   (means[:, 0, :].unsqueeze(1).repeat(
                       1, sample_length, 1)))
+        attn = self.attn(enc_out)                                          # attn: [3B, win_size, 1]
+        attn_score = torch.softmax(attn, dim=1)                            # attn_score: [3B, win_size, 1]
         if self.configs.contrastive == 1:
-            attn = self.attn(enc_out)                                          # attn: [3B, win_size, 1]
-            attn_score = torch.softmax(attn, dim=1)                            # attn_score: [3B, win_size, 1]
             return enc_out, dec_out, attn_score
         return dec_out
 
