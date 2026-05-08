395dde531c42d976331529a1a3c1a0884c18aba3
HDoan043 <165142987+HDoan043@users.noreply.github.com>
2026-05-09 01:31:17 +0700

    Fix anomaly list filtering logic

--- data_provider/data_loader.py
+++ data_provider/data_loader.py
@@ -416,9 +416,9 @@ class Dataset_Custom(Dataset):
             with open(self.args.anomaly_list, "r", encoding="utf-8") as f:
                 self.anomaly_ls = json.load(f)
         
-            self.real_anomaly_ls = [each for each in self.anomaly_ls if "FALSE ANOMALY (LABEL 0)" in anomaly or "HARD NEGATIVE - MUST LABEL AS 0" in anomaly\
-                                                        or "FALSE ANOMALY (LABEL 0)" in anomaly or "FALSE ANOMALY (LABEL 0)" in anomaly\
-                                                            or "LABEL 0" in anomaly or "LABEL AS 0" in each["anomaly"]]
+            self.real_anomaly_ls = [each for each in self.anomaly_ls if "FALSE ANOMALY (LABEL 0)" in each["anomaly"] or "HARD NEGATIVE - MUST LABEL AS 0" in each["anomaly"]\
+                                                        or "FALSE ANOMALY (LABEL 0)" in each["anomaly"] or "FALSE ANOMALY (LABEL 0)" in each["anomaly"]\
+                                                            or "LABEL 0" in each["anomaly"] or "LABEL AS 0" in each["anomaly"]]
             self.fake_anomaly_ls = [each for each in self.anomaly_ls if each not in real_anomaly_ls]
             with open(self.args.name_id_map, "r") as f:
                 self.name_id_map = json.load(f)
