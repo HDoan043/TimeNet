59a6e8ecdd385acc5fcef191d276d87d885fbb1a
HDoan043 <165142987+HDoan043@users.noreply.github.com>
2026-05-08 19:17:56 +0700

    Fix formatting of augmentation_config argument help text

--- run.py
+++ run.py
@@ -189,7 +189,7 @@ if __name__ == '__main__':
     parser.add_argument('--neighbor_sim_pos', type=int, default=0, help='number of augmentations of neighbors that are considered as positive samples of an anchor in contrastive learning')
     parser.add_argument('--temperature', type=float, default=1, help='temperature of smoothing the softmax')
     # positive samples generating control
-    parser.add_argument('--augmentation_config', type=str, default=str(augmentation_config), help='config of augmentation to generating positive samples for contrastive learning")
+    parser.add_argument('--augmentation_config', type=str, default=str(augmentation_config), help='config of augmentation to generating positive samples for contrastive learning')
     parser.add_argument('--emphasize_negative', type=float, default=0.5, help='how the true negative samples are emphasized more than the fake ones')
     parser.add_argument('--contrastive_weight', type=float, default=0.1, help='ratio of the contrastive loss')
     
