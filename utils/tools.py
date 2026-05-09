629fe58a96587c751c6dedb1956714aa23488e38
HDoan043 <165142987+HDoan043@users.noreply.github.com>
2026-05-10 00:05:03 +0700

    Add visualize_anomaly function for energy data
    Added a function to visualize anomalies in energy data.

--- utils/tools.py
+++ utils/tools.py
@@ -148,3 +148,8 @@ def adjustment(gt, pred):
 
 def cal_accuracy(y_pred, y_true):
     return np.mean(y_pred == y_true)
+
+def visualize_anomaly(test_energy, ground_truth, timestamp):
+    '''
+    test_energy:
+    '''
