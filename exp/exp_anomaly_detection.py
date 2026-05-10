81e3ca0c3970bf2e2db553057abd8e3970e6d088
HDoan043 <165142987+HDoan043@users.noreply.github.com>
2026-05-10 11:20:00 +0700

    Update timestamps to use batch_y_mark
    Changed timestamps to use batch_y_mark instead of batch_x_mark.

--- exp/exp_anomaly_detection.py
+++ exp/exp_anomaly_detection.py
@@ -271,7 +271,7 @@ class Exp_Anomaly_Detection(Exp_Basic):
                 print("score: {}".format(score.shape))
                 print("batch_y: {}".format(batch_y.shape))
             test_labels.append(batch_y)
-            timestamps.append(batch_x_mark.detach().cpu().numpy())
+            timestamps.append(batch_y_mark.detach().cpu().numpy())
         
         attens_energy = np.concatenate(attens_energy, axis=0)                        # attens_energy: [batch_size*num_batch x win_size]
         ######################################
