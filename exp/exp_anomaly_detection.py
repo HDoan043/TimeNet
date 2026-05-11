b284f82de5780dc8b1c05ed93123b6dd7d63586e
HDoan043 <165142987+HDoan043@users.noreply.github.com>
2026-05-11 22:21:51 +0700

    Modify optimizer selection for contrastive training

--- exp/exp_anomaly_detection.py
+++ exp/exp_anomaly_detection.py
@@ -36,7 +36,30 @@ class Exp_Anomaly_Detection(Exp_Basic):
         return data_set, data_loader
 
     def _select_optimizer(self):
-        model_optim = optim.Adam(self.model.parameters(), lr=self.args.learning_rate)
+        if self.args.contrastive == 1 and self.args.is_training == 1:
+            base_params = []
+            projector_params = list(self.model.contrastive_project.parameters())
+            
+            projector_param_ids = {
+                id(p) for p in projector_params
+            }
+            
+            for p in self.model.parameters():
+                if id(p) not in projector_param_ids:
+                    base_params.append(p)
+            
+            optimizer = AdamW([
+                {
+                    "params": base_params,
+                    "lr": self.args.learning_rate
+                },
+                {
+                    "params": projector_params,
+                    "lr": self.args.learning_rate*100
+                }
+            ])
+        else:
+            model_optim = optim.Adam(self.model.parameters(), lr=self.args.learning_rate)
         return model_optim
 
     def _select_criterion(self):
