172f1a8c72ddb2bfd36493e2884fc7beab219e62
HDoan043 <165142987+HDoan043@users.noreply.github.com>
2026-05-11 17:26:35 +0700

    Use nn.functional for conv1d in losses.py

--- utils/losses.py
+++ utils/losses.py
@@ -264,6 +264,6 @@ class TripletLoss(nn.Module):
         labels = labels.unsqueeze(1)
         padding = kernel_size // 2
         
-        blurred = F.conv1d( labels, kernel, padding=padding)
+        blurred = nn.functional.conv1d( labels, kernel, padding=padding)
         
         return blurred.squeeze(1)
