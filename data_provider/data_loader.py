5b2e7a4ac87c0aa0587e78db571f64aea59f05ea
HDoan043 <165142987+HDoan043@users.noreply.github.com>
2026-05-12 13:19:51 +0700

    Refactor positive sampling and augmentation methods
    Refactor positive sampling methods and update augmentation logic.

--- data_provider/data_loader.py
+++ data_provider/data_loader.py
@@ -402,7 +402,7 @@ class Dataset_Custom(Dataset):
 
         # =========================== CONTRASTIVE LEARNING ==============================
         if self.args.contrastive == 1 and self.set_type==0:
-            
+            # Augmentation define
             try:
                 self.augmentation_config = json.loads(self.args.augmentation_config)
             except:
@@ -417,6 +417,9 @@ class Dataset_Custom(Dataset):
                     "jitter": {"ratio": 0.10, "parameter":{"sigma": (0.003,0.01), "clip": (0.01, 0.03)}},
                     "random_guided_warp_5g": {"ratio": 0.20, "parameter": {}}
                 }
+            # Stochastic define
+            self.lags, self.scores = get_periodic_lags(self.data_x, win_size=self.args.win_size)
+            
             with open(self.args.anomaly_list, "r", encoding="utf-8") as f:
                 self.anomaly_ls = json.load(f)
         
@@ -463,68 +466,23 @@ class Dataset_Custom(Dataset):
                 negative = self.scaler.transform(negative)
         
                 # Gen Posivie sample
-                if np.random.rand() <0.3 and len(self.fake_anomaly_ls)>0:
-                    anomaly_ls = np.random.choice(self.fake_anomaly_ls, 1)
-                    positive, label = inject_full(raw_window, anomaly_ls, self.position_map, self.name_id_map)
-                    positive = self.scaler.transform(positive)
-                    seq_x = (seq_x, positive, negative, label)
-                    return seq_x, seq_y, seq_x_mark, seq_y_mark
-                    
-                raw_window = raw_window.values
-                positive = seq_x.copy()
-
-                neighbors = []
-
-                offsets = np.random.choice(
-                    [o for o in range(-24, 25) if o != 0],
-                    size=6,
-                    replace=False
-                )
-                
-                for offset in offsets:
-                    start = x_index_start + offset
-                    end = x_index_end + offset
-                
-                    if start >= 0 and end <= len(self.data_x):
-                        neighbors.append( self.data_x[start:end].copy())
-                
-                positive = self.inverse_transform(positive)
-                neighbors = [self.inverse_transform(neighbor) for neighbor in neighbors]
-
-                self.augmentation_config["random_guided_warp_5g"]["parameter"]["neighbors"] = neighbors
-
+                # if np.random.rand() <0.3 and len(self.fake_anomaly_ls)>0:
+                #     anomaly_ls = np.random.choice(self.fake_anomaly_ls, 1)
+                #     positive, label = inject_full(raw_window, anomaly_ls, self.position_map, self.name_id_map)
+                #     positive = self.scaler.transform(positive)
+                #     seq_x = (seq_x, positive, negative, label)
+                #     return seq_x, seq_y, seq_x_mark, seq_y_mark
                 r = np.random.rand()
-                aug_ls = []
-                if r<0.7: num_aug = 1
-                elif r<0.95: num_aug = 2
-                else: num_aug = 3
-
-                if num_aug == 1:
-                    aug_ls = list(self.augmentation_config.keys())
-                    p = [self.augmentation_config[k]["ratio"] for k in aug_ls]
-                    aug_ls = np.random.choice(aug_ls, num_aug, p=p)
+                if r < 0.7:
+                    positive = self.stochastic_positive_sampler(x_index_start)
+                elif r<0.95:
+                    neighbor_distance = 1
+                    neighbor_candidates = [self.data_x[j: j + self.args.win_size].copy() for j in range(max(0,x_index_start - neighbor_distance), 
+                                                                                                min(len(self.data_x),x_index_end + neighbor_distance)) if j !=x_index_start ]
+                    positive = np.random.choice(neighbor_candidates)
                 else:
-                    temporal_augs = ["time_warp", "window_warp_5g", "random_guided_warp_5g", "window_slice"]
-                    temporal_p = np.array([self.augmentation_config[k]["ratio"] for k in temporal_augs])
-                    temporal_p = temporal_p / temporal_p.sum()
-                    magnitude_augs = ["jitter", "scaling", "magnitude_warp"]
-                    magnitude_p = np.array([self.augmentation_config[k]["ratio"] for k in magnitude_augs])
-                    magnitude_p = magnitude_p / magnitude_p.sum()
-                    temporal_aug = np.random.choice(temporal_augs, p=temporal_p)
-                    aug_ls.append(temporal_aug)
-                    aug_ls.extend(np.random.choice(magnitude_augs, num_aug - 1, p=magnitude_p))
-
-                for aug in aug_ls:
-                    aug_func = getattr(new_augmentation, aug)
-                    parameters = self.augmentation_config[aug]["parameter"]
-                    positive = aug_func(positive, **parameters)
-                window_min = raw_window.min(axis=0)
-                window_max = raw_window.max(axis=0)
+                    positive = self.augmentation_positive_sampler(seq_x, x_index_start, x_index_end)
                 
-                margin = 0.15 * (window_max - window_min)
-                positive = np.clip( positive, window_min - margin, window_max + margin)
-                positive = self.scaler.transform(positive)
-
                 assert positive.shape == seq_x.shape
                 assert not np.isnan(positive).any()
                 assert not np.isinf(positive).any()
@@ -569,11 +527,72 @@ class Dataset_Custom(Dataset):
         # Trả về định dạng string để bạn dễ đọc/lưu log
         return dt_series.dt.strftime('%Y-%m-%d %H:%M:%S').values
 
-    def stochastic_positive_sampler(i, lags, scores, data, win_size, jitter_range=(-2, 2)):
+    def augmentation_positive_sampler(self, seq_x, x_index_start, x_index_end):
+        positive = seq_x.copy()
+
+        neighbors = []
+
+        offsets = np.random.choice(
+            [o for o in range(-24, 25) if o != 0],
+            size=6,
+            replace=False
+        )
+        
+        for offset in offsets:
+            start = x_index_start + offset
+            end = x_index_end + offset
+        
+            if start >= 0 and end <= len(self.data_x):
+                neighbors.append( self.data_x[start:end].copy())
+        
+        positive = self.inverse_transform(positive)
+        neighbors = [self.inverse_transform(neighbor) for neighbor in neighbors]
+
+        self.augmentation_config["random_guided_warp_5g"]["parameter"]["neighbors"] = neighbors
+
+        r = np.random.rand()
+        aug_ls = []
+        if r<0.7: num_aug = 1
+        elif r<0.95: num_aug = 2
+        else: num_aug = 3
+
+        if num_aug == 1:
+            aug_ls = list(self.augmentation_config.keys())
+            p = [self.augmentation_config[k]["ratio"] for k in aug_ls]
+            aug_ls = np.random.choice(aug_ls, num_aug, p=p)
+        else:
+            temporal_augs = ["time_warp", "window_warp_5g", "random_guided_warp_5g", "window_slice"]
+            temporal_p = np.array([self.augmentation_config[k]["ratio"] for k in temporal_augs])
+            temporal_p = temporal_p / temporal_p.sum()
+            magnitude_augs = ["jitter", "scaling", "magnitude_warp"]
+            magnitude_p = np.array([self.augmentation_config[k]["ratio"] for k in magnitude_augs])
+            magnitude_p = magnitude_p / magnitude_p.sum()
+            temporal_aug = np.random.choice(temporal_augs, p=temporal_p)
+            aug_ls.append(temporal_aug)
+            aug_ls.extend(np.random.choice(magnitude_augs, num_aug - 1, p=magnitude_p))
+
+        for aug in aug_ls:
+            aug_func = getattr(new_augmentation, aug)
+            parameters = self.augmentation_config[aug]["parameter"]
+            positive = aug_func(positive, **parameters)
+        window_min = raw_window.min(axis=0)
+        window_max = raw_window.max(axis=0)
+        
+        margin = 0.15 * (window_max - window_min)
+        positive = np.clip( positive, window_min - margin, window_max + margin)
+        positive = self.scaler.transform(positive)
+
+        return positive
+        
+    def stochastic_positive_sampler(self, i, jitter_range=(-2, 2)):
         """
         Lấy mẫu dương theo phương pháp Stochastic (Xác suất) kết hợp Jitter.
         """
+        data = self.data_x
         T = data.shape[0]
+        lags = self.lags
+        scores = self.scores
+        win_size = self.args.win_size
         
         # Lọc các lag không bị tràn mảng (Out of bounds)
         valid_indices = [idx for idx, lag in enumerate(lags) if i + lag <= T - win_size]
