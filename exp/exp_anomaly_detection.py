a1aa9b9ded2b7804916414860e77d3c8d4b55562
HDoan043 <165142987+HDoan043@users.noreply.github.com>
2026-05-21 19:10:41 +0700

    Refactor training loop and logging for clarity

--- exp/exp_anomaly_detection.py
+++ exp/exp_anomaly_detection.py
@@ -187,8 +187,9 @@ class Exp_Anomaly_Detection(Exp_Basic):
             epoch_time = time.time()
             pbar = ProgressBar(train_loader, bin=60)
             i = 0
-            for batch in train_loader:
-            # for batch in pbar:
+            epoch_logs = {"loss_recon": 0, "loss_contrastive": 0, "recon_gap": 0, "throttle": 0, "alpha_sim": 0, "alpha_mag": 0}
+            # for batch in train_loader:
+            for batch in pbar:
                 aggregate_steps += 1
                 iter_count += 1
                 model_optim.zero_grad()
@@ -206,33 +207,44 @@ class Exp_Anomaly_Detection(Exp_Basic):
                     batch_x_mark = batch_all_mark.to(self.device)
                     
                     hidden_state, outputs, attn_pooling = self.model(batch_x, batch_x_mark, None, None)
-                    loss = criterion(batch_x, outputs, hidden_state, idx, pos_idx, neg_idx, label, attn_pooling, batch_base_mse)
+                    loss, metrics = criterion(batch_x, outputs, hidden_state, idx, pos_idx, neg_idx, label, attn_pooling, batch_base_mse)
                     train_loss.append(loss.item())
 
-                # speed = (time.time() - time_begin) / aggregate_steps
-                # left_time_s = speed * ((self.args.train_epochs - epoch) * train_steps - i)
-                # if left_time_s <60: 
-                #     left_time = f"{round(left_time_s,4)}s"
-                # elif left_time_s<3600:
-                #     left_time = f"{round(left_time_s/60,4)}mins"
-                # else: left_time = f"{round(left_time_s/3600,4)}hs"
+                    for key in epoch_logs.keys():
+                        epoch_logs[key] += metrics[key]
+
+                speed = (time.time() - time_begin) / aggregate_steps
+                left_time_s = speed * ((self.args.train_epochs - epoch) * train_steps - i)
+                if left_time_s <60: 
+                    left_time = f"{round(left_time_s,4)}s"
+                elif left_time_s<3600:
+                    left_time = f"{round(left_time_s/60,4)}mins"
+                else: left_time = f"{round(left_time_s/3600,4)}hs"
     
-                # pbar.set_postfix(
-                #     {
-                #         "Epoch": epoch + 1,
-                #         "Iteration": f"{i+1}/{train_steps}",
-                #         "Loss": loss.item(),
-                #         "Speed": f"{round(speed, 4)}s/iter",
-                #         "Left time": left_time
-                #     }
-                # )
-                if (i + 1) % 100 == 0:
-                    print("\titers: {0}, epoch: {1} | loss: {2:.7f}".format(i + 1, epoch + 1, loss.item()))
-                    speed = (time.time() - time_now) / iter_count
-                    left_time = speed * ((self.args.train_epochs - epoch) * train_steps - i)
-                    print('\tspeed: {:.4f}s/iter; left time: {:.4f}s'.format(speed, left_time))
-                    iter_count = 0
-                    time_now = time.time()
+                pbar.set_postfix(
+                    {
+                        "Epoch": epoch + 1,
+                        "Iteration": f"{i+1}/{train_steps}",
+                        "Loss": loss.item(),
+                        "Speed": f"{round(speed, 4)}s/iter",
+                        "Left time": left_time
+                    }
+                )
+                # Sau khi kết thúc vòng lặp Batch (Hết 1 Epoch):
+        
+            num_batches = len(train_loader)
+            for key in epoch_logs.keys():
+                epoch_logs[key] /= num_batches # Tính trung bình cả Epoch
+            
+            # In ra màn hình hoặc ghi vào file log:
+            print(f"\nEpoch {epoch} | Recon: {epoch_logs['loss_recon']:.4f} | Cont: {epoch_logs['loss_contrastive']:.4f} | Gap: {epoch_logs['recon_gap']:.4f} | Throttle: {epoch_logs['throttle']:.4f}")
+                # if (i + 1) % 100 == 0:
+                #     print("\titers: {0}, epoch: {1} | loss: {2:.7f}".format(i + 1, epoch + 1, loss.item()))
+                #     speed = (time.time() - time_now) / iter_count
+                #     left_time = speed * ((self.args.train_epochs - epoch) * train_steps - i)
+                #     print('\tspeed: {:.4f}s/iter; left time: {:.4f}s'.format(speed, left_time))
+                #     iter_count = 0
+                #     time_now = time.time()
 
                 loss.backward()
                 model_optim.step()
