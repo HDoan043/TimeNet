981f8c15c2538dd086e35d67108ebc2a0be4a30f
HDoan043 <165142987+HDoan043@users.noreply.github.com>
2026-05-24 10:46:53 +0700

    Update exp_supervise_5g_network.py

--- exp/exp_supervise_5g_network.py
+++ exp/exp_supervise_5g_network.py
@@ -23,7 +23,7 @@ warnings.filterwarnings('ignore')
 
 class Exp_Supervise_5G_Network(Exp_Basic):
     def __init__(self, args):
-        super(Exp_Anomaly_Detection, self).__init__(args)
+        super(Exp_Supervise_5G_Network, self).__init__(args)
 
     def _build_model(self):
         model = self.model_dict[self.args.model].Model(self.args).float()
