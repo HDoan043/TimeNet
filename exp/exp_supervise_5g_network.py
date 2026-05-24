577cbbf06a0eb55178cb23322cfdcf8956e641b5
HDoan043 <165142987+HDoan043@users.noreply.github.com>
2026-05-24 11:00:37 +0700

    Update exp_supervise_5g_network.py

--- exp/exp_supervise_5g_network.py
+++ exp/exp_supervise_5g_network.py
@@ -33,7 +33,7 @@ class Exp_Supervise_5G_Network(Exp_Basic):
     
         return model
 
-    def _get_data(self, flag, contrastive=False):
+    def _get_data(self, flag, contrastive=False, phase='test'):
         data_set, data_loader = data_provider(self.args, flag, contrastive=contrastive)
         return data_set, data_loader
 
