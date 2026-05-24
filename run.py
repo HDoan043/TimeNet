5888d6f83706cff3fe7b123f5d27ccd491b08823
HDoan043 <165142987+HDoan043@users.noreply.github.com>
2026-05-24 16:37:23 +0700

    Update run.py

--- run.py
+++ run.py
@@ -191,7 +191,7 @@ if __name__ == '__main__':
     parser.add_argument('--base_model', type=str, default="TimesNet", help="base pretrained model for supervisin 5g network: TimesNet")
     parser.add_argument('--base_model_checkpoint', type=str, default="", help="path to the pretrained checkpoint of base model")
     parser.add_argument('--corrector_criterion', type=str, default="MILLoss", help="which criterion is used in training corrector")
-
+    parser.add_argument('--top_k_ratio', type=float, default=0.2, help="top k ratio in MIL Loss")
     # Contrastive
     parser.add_argument('--contrastive', type=int, default=0, help='1: contrastive learning, 0: unsupervised learning')
     parser.add_argument('--contrastive_criterion', type=str, default="NTXent", help='NTXent: use NTXent Loss, Triplet: use Triplet')
