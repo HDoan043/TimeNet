456b82e46df88400a84bc0297ef8f9e84d6510c1
HDoan043 <165142987+HDoan043@users.noreply.github.com>
2026-06-10 07:54:13 +0700

    Refactor training loop to use ProgressBar

--- exp/exp_anomaly_detection.py
+++ exp/exp_anomaly_detection.py
@@ -175,8 +175,8 @@ class Exp_Anomaly_Detection(Exp_Basic):
             pbar = ProgressBar(train_loader, bin=60)
             i = 0
             epoch_logs = {"loss_reconstruct": 0, "loss_contrastive": 0}
-            for batch in train_loader:
-            # for batch in pbar:
+            # for batch in train_loader:
+            for batch in pbar:
                 aggregate_steps += 1
                 iter_count += 1
                 model_optim.zero_grad()
@@ -202,32 +202,32 @@ class Exp_Anomaly_Detection(Exp_Basic):
                             epoch_logs[key] += metrics[key]
                     train_loss.append(loss.item())
 
-                # speed = (time.time() - time_begin) / aggregate_steps
-                # left_time_s = speed * ((self.args.train_epochs - epoch) * train_steps - i)
-                # if left_time_s <60: 
-                #     left_time = f"{round(left_time_s,4)}s"
-                # elif left_time_s<3600:
-                #     left_time = f"{round(left_time_s/60,4)}mins"
-                # else: left_time = f"{round(left_time_s/3600,4)}hs"
+                speed = (time.time() - time_begin) / aggregate_steps
+                left_time_s = speed * ((self.args.train_epochs - epoch) * train_steps - i)
+                if left_time_s <60: 
+                    left_time = f"{round(left_time_s,4)}s"
+                elif left_time_s<3600:
+                    left_time = f"{round(left_time_s/60,4)}mins"
+                else: left_time = f"{round(left_time_s/3600,4)}hs"
     
-                # pbar.set_postfix(
-                #     {
-                #         "Epoch": epoch + 1,
-                #         "Iteration": f"{i+1}/{train_steps}",
-                #         "Loss": loss.item(),
-                #         "Speed": f"{round(speed, 4)}s/iter",
-                #         "Left time": left_time
-                #     }
-                # )
+                pbar.set_postfix(
+                    {
+                        "Epoch": epoch + 1,
+                        "Iteration": f"{i+1}/{train_steps}",
+                        "Loss": loss.item(),
+                        "Speed": f"{round(speed, 4)}s/iter",
+                        "Left time": left_time
+                    }
+                )
                 # Sau khi kết thúc vòng lặp Batch (Hết 1 Epoch):
         
-                if (i + 1) % 100 == 0:
-                    print("\titers: {0}, epoch: {1} | loss: {2:.7f}".format(i + 1, epoch + 1, loss.item()))
-                    speed = (time.time() - time_now) / iter_count
-                    left_time = speed * ((self.args.train_epochs - epoch) * train_steps - i)
-                    print('\tspeed: {:.4f}s/iter; left time: {:.4f}s'.format(speed, left_time))
-                    iter_count = 0
-                    time_now = time.time()
+                # if (i + 1) % 100 == 0:
+                #     print("\titers: {0}, epoch: {1} | loss: {2:.7f}".format(i + 1, epoch + 1, loss.item()))
+                #     speed = (time.time() - time_now) / iter_count
+                #     left_time = speed * ((self.args.train_epochs - epoch) * train_steps - i)
+                #     print('\tspeed: {:.4f}s/iter; left time: {:.4f}s'.format(speed, left_time))
+                #     iter_count = 0
+                #     time_now = time.time()
 
                 loss.backward()
                 t.nn.utils.clip_grad_norm_(self.model.parameters(), max_norm=1.0)
